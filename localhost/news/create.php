9286fb23ebb413f6df6cf8558bf19699f71ed75d
SmailFun <98034374+SmailFun@users.noreply.github.com>
2022-11-25 17:54:39 +0300

    Update create.php

--- localhost/news/create.php
+++ localhost/news/create.php
@@ -6,29 +6,7 @@
 </head>
 
 <body>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Ошибка подключения: " . $conn->connect_error);
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $about = $_POST['about'];
-
-    $sql = "INSERT INTO myNews (name, about) VALUES('". $name ."', '". $about ."')";
-
-    $conn->query($sql);
-
-    $conn->close();
-}
-
-?>
 
 <form name="form1" method="post" action="confirm">
     <table>
