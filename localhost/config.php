e44cf2b32264a49f93428002b07c5534104a2f2c
SmailFun <98034374+SmailFun@users.noreply.github.com>
2022-11-25 16:46:13 +0300

    Add files via upload

--- localhost/config.php
+++ localhost/config.php
@@ -1,7 +1,9 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "myDB";
 
 
 // Create connection
@@ -10,22 +12,4 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $sql = "CREATE DATABASE myDB";
-
-
-
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode('/', trim($uri, '/'));
-$method = $_SERVER['REQUEST_METHOD'];
-
-
-$file = 'news/' . $segments[0] . '.php';
-
-if(file_exists($file))
-    require $file;
-else
-    require 'pages/404.php';
-
-
